4da80f304767e0cb35f9bd3405577d3e14f37977
Camilo Merino <camilo.merino.m@gmail.com>
2026-03-08 01:27:18 -0300

    fix next16 params

--- app/[Slug]/page.tsx
+++ app/[Slug]/page.tsx
@@ -1,8 +1,14 @@
-export default function Page({ params }: { params: { slug: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
   return (
     <main style={{ padding: 40 }}>
       <h1>Slug detectado:</h1>
-      <p>{params.slug}</p>
+      <p>{slug}</p>
     </main>
   );
 }
