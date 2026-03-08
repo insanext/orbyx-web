dfaa5c5d2fc69bbc53bca53d9a2c079173990aa2
Camilo Merino <camilo.merino.m@gmail.com>
2026-03-08 01:42:56 -0300

    debug params

--- app/[Slug]/page.tsx
+++ app/[Slug]/page.tsx
@@ -4,12 +4,11 @@ import { useParams } from "next/navigation";
 
 export default function Page() {
   const params = useParams();
-  const slug = params?.slug;
 
   return (
     <main style={{ padding: 40 }}>
-      <h1>Slug detectado:</h1>
-      <p>{typeof slug === "string" ? slug : ""}</p>
+      <h1>Params completos:</h1>
+      <pre>{JSON.stringify(params, null, 2)}</pre>
     </main>
   );
 }
